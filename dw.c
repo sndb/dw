72fe4e9baa495e365c20417fe9279d2a1c98619c
sndb <sndb@sndb.xyz>
2023-10-30 02:18:41 +0300

    use arc4random_uniform, simplify

--- dw.c
+++ dw.c
@@ -1,35 +1,21 @@
+#include "words.h"
+
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/random.h>
 
-#include "words.h"
-
-const int DEFAULT_NWORDS = 4;
-
-size_t
-randsize(void)
-{
-	uint64_t n;
-	ssize_t r = getrandom((void *)&n, sizeof(size_t), 0);
-	if (r != sizeof(size_t)) {
-		fprintf(stderr, "dw: cannot read random bytes\n");
-		exit(EXIT_FAILURE);
-	}
-	return n;
-}
+#define LENGTH(x) (sizeof(x) / sizeof(x[0]))
 
 const char *
 randword(void)
 {
-	size_t i = randsize() % (sizeof(words) / sizeof(words[0]));
-	return words[i];
+	return words[arc4random_uniform(LENGTH(words))];
 }
 
 int
 main(int argc, char *argv[])
 {
-	int nwords = DEFAULT_NWORDS;
+	int nwords = 4;
 	if (argc >= 2)
 		nwords = atoi(argv[1]);
 	if (nwords == 0) {
@@ -38,5 +24,5 @@ main(int argc, char *argv[])
 	}
 	for (int i = 0; i < nwords; i++)
 		printf("%s%s", randword(), i == nwords - 1 ? "\n" : " ");
-	return 0;
+	return EXIT_SUCCESS;
 }
